747fbc76d8cba5227ba524b46420f68f1c477076
hulukaihuaba <15700086047@163.com>
2015-01-08 14:12:33 +0800

    首次在手机上跑

--- src/com/demo/androiditent/MainActivity.java
+++ src/com/demo/androiditent/MainActivity.java
@@ -3,14 +3,19 @@ package com.demo.androiditent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		TextView textView = (TextView) findViewById(R.id.textview1);
+		EditText editText = (EditText) findViewById(R.id.editext1);
+		textView.setText("ÓÃ»§Ãû: ");
+		editText.setText("Shenguofeng");
 	}
 
 	@Override
